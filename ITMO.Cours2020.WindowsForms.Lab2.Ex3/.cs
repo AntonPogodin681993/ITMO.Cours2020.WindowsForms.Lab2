e351354bd71db863ae07d137f6166d44a92efaca
Anton Pogodin <anton.pogodin68@bk.ru>
2020-05-25 20:45:59 +0300

    Add project files.

---
+++ ITMO.Cours2020.WindowsForms.Lab2.Ex3/.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.Cours2020.WindowsForms.Lab2.Ex3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ITMO.Cours2020.WindowsForms.Lab2.Ex6/IPubs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ITMO.Cours2020.WindowsForms.Lab2.Ex6
+{
+    interface IPubs
+    {
+        void Subs();
+        bool IfSubs { get; set;}
+
+        
+    }
+}
